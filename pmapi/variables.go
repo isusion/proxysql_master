7f7b18fb03b0bcec2eaa05fcb3de4a4202bf4e17
Lei Tian <taylor840326@gmail.com>
2017-12-26 16:15:06 +0800

    update UpdateOneConfig ,catch key not found exception

--- pmapi/variables.go
+++ pmapi/variables.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/imSQL/proxysql"
+	"github.com/juju/errors"
 )
 
 //查询出所有变量的内容
@@ -16,44 +17,35 @@ func (pmapi *PMApi) ListPsVariables(c *gin.Context) {
 	port := c.Query("port")
 	username := c.Query("adminuser")
 	password := c.Query("adminpass")
-	/*
-		limit, _ := strconv.ParseInt(c.Query("limit"), 10, 64)
-		page, _ := strconv.ParseInt(c.Query("page"), 10, 64)
 
-		if limit == 0 {
-			limit = 10
-		}
-
-		if page == 0 {
-			page = 1
-		}
-
-		skip := (page - 1) * limit
-	*/
-
-	if len(hostname) == 0 || hostname == "undefined" {
-		c.JSON(http.StatusOK, []proxysql.Variables{})
+	if hostname == "" || hostname == "undefined" || port == "" || port == "undefined" || username == "" || username == "undefined" || password == "" || password == "undefined" {
+		c.JSON(http.StatusBadRequest, errors.ErrorStack(errors.New("hostname|port|adminuser|adminpass length is 0 or value is undefined")))
 	} else {
+
 		pmapi.PMhost = hostname
 		pmapi.PMport, _ = strconv.ParseUint(port, 10, 64)
 		pmapi.PMuser = username
 		pmapi.PMpass = password
 
+		// New connection instance.
 		pmapi.PMconn, pmapi.ApiErr = proxysql.NewConn(pmapi.PMhost, pmapi.PMport, pmapi.PMuser, pmapi.PMpass)
 		if pmapi.ApiErr != nil {
-			c.JSON(http.StatusExpectationFailed, gin.H{"error": pmapi.ApiErr})
-		}
+			c.JSON(http.StatusInternalServerError, errors.ErrorStack(pmapi.ApiErr))
+		} else {
+			// Open Connection.
+			pmapi.Apidb, pmapi.ApiErr = pmapi.PMconn.OpenConn()
+			if pmapi.ApiErr != nil {
+				c.JSON(http.StatusInternalServerError, errors.ErrorStack(pmapi.ApiErr))
+			} else {
 
-		pmapi.Apidb, pmapi.ApiErr = pmapi.PMconn.OpenConn()
-		if pmapi.ApiErr != nil {
-			c.JSON(http.StatusExpectationFailed, gin.H{"error": pmapi.ApiErr})
+				aryvars, pmapi.ApiErr = proxysql.GetConfig(pmapi.Apidb)
+				if pmapi.ApiErr != nil {
+					c.JSON(http.StatusExpectationFailed, errors.ErrorStack(pmapi.ApiErr))
+				} else {
+					c.JSON(http.StatusOK, aryvars)
+				}
+			}
 		}
-
-		aryvars, pmapi.ApiErr = proxysql.GetConfig(pmapi.Apidb)
-		if pmapi.ApiErr != nil {
-			c.JSON(http.StatusExpectationFailed, gin.H{"error": pmapi.ApiErr})
-		}
-		c.JSON(http.StatusOK, aryvars)
 	}
 
 }
@@ -67,31 +59,45 @@ func (pmapi *PMApi) UpdateOneVariables(c *gin.Context) {
 	username := c.Query("adminuser")
 	password := c.Query("adminpass")
 
-	if len(hostname) == 0 || hostname == "undefined" {
-		c.JSON(http.StatusOK, []proxysql.Variables{})
+	if hostname == "" || hostname == "undefined" || port == "" || port == "undefined" || username == "" || username == "undefined" || password == "" || password == "undefined" {
+		c.JSON(http.StatusBadRequest, errors.ErrorStack(errors.New("hostname|port|adminuser|adminpass length is 0 or value is undefined")))
 	} else {
-		pmapi.PMhost = hostname + ":" + port
+
+		pmapi.PMhost = hostname
+		pmapi.PMport, _ = strconv.ParseUint(port, 10, 64)
 		pmapi.PMuser = username
 		pmapi.PMpass = password
 
+		// New connection instance.
 		pmapi.PMconn, pmapi.ApiErr = proxysql.NewConn(pmapi.PMhost, pmapi.PMport, pmapi.PMuser, pmapi.PMpass)
 		if pmapi.ApiErr != nil {
-			c.JSON(http.StatusExpectationFailed, gin.H{"error": pmapi.ApiErr})
-		}
+			c.JSON(http.StatusInternalServerError, errors.ErrorStack(pmapi.ApiErr))
+		} else {
+			// Open Connection.
+			pmapi.Apidb, pmapi.ApiErr = pmapi.PMconn.OpenConn()
+			if pmapi.ApiErr != nil {
+				c.JSON(http.StatusInternalServerError, errors.ErrorStack(pmapi.ApiErr))
+			} else {
 
-		pmapi.Apidb, pmapi.ApiErr = pmapi.PMconn.OpenConn()
-		if pmapi.ApiErr != nil {
-			c.JSON(http.StatusExpectationFailed, gin.H{"error": pmapi.ApiErr})
-		}
-		if pmapi.ApiErr = c.Bind(&tmpvars); pmapi.ApiErr != nil {
-			c.JSON(http.StatusExpectationFailed, gin.H{"result": pmapi.ApiErr})
-		}
+				if pmapi.ApiErr = c.Bind(&tmpvars); pmapi.ApiErr != nil {
+					c.JSON(http.StatusExpectationFailed, gin.H{"result": pmapi.ApiErr})
+				} else {
+					pmapi.ApiErr = proxysql.UpdateOneConfig(pmapi.Apidb, tmpvars.VariablesName, tmpvars.Value)
+					if pmapi.ApiErr != nil {
+						switch {
+						case errors.IsNotImplemented(pmapi.ApiErr):
+							c.JSON(http.StatusNotImplemented, errors.ErrorStack(pmapi.ApiErr))
+						default:
+							c.JSON(http.StatusExpectationFailed, errors.ErrorStack(pmapi.ApiErr))
+						}
+					} else {
+						c.JSON(http.StatusOK, gin.H{"exit": "0", "messages": tmpvars.VariablesName + " Update Successed!"})
+					}
 
-		pmapi.ApiErr = proxysql.UpdateOneConfig(pmapi.Apidb, tmpvars.VariablesName, tmpvars.Value)
-		if pmapi.ApiErr != nil {
-			c.JSON(http.StatusExpectationFailed, gin.H{"result": "UpdateOneVariable Failed"})
+				}
+
+			}
+
 		}
-		c.JSON(http.StatusOK, gin.H{"result": "OK"})
 	}
-
 }
