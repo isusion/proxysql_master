a83d73cf6400b760c60d855b479b9efe4c99e946
Lei Tian <taylor840326@gmail.com>
2017-12-26 10:44:13 +0800

    update CreateOneUser/UpdateOneUser/ListAllUsers functions

--- pmapi/users.go
+++ pmapi/users.go
@@ -61,8 +61,8 @@ func (pmapi *PMApi) CreateOneUser(c *gin.Context) {
 	username := c.Query("adminuser")
 	password := c.Query("adminpass")
 
-	if len(hostname) == 0 {
-		c.JSON(http.StatusOK, []proxysql.Users{})
+	if hostname == "" || hostname == "undefined" || port == "" || port == "undefined" || username == "" || username == "undefined" || password == "" || password == "undefined" {
+		c.JSON(http.StatusBadRequest, errors.ErrorStack(errors.New("hostname|port|adminuser|adminpass length is 0 or value is undefined")))
 	} else {
 		pmapi.PMhost = hostname
 		pmapi.PMport, _ = strconv.ParseUint(port, 10, 64)
@@ -71,26 +71,28 @@ func (pmapi *PMApi) CreateOneUser(c *gin.Context) {
 
 		pmapi.PMconn, pmapi.ApiErr = proxysql.NewConn(pmapi.PMhost, pmapi.PMport, pmapi.PMuser, pmapi.PMpass)
 		if pmapi.ApiErr != nil {
-			c.JSON(http.StatusExpectationFailed, gin.H{"error": pmapi.ApiErr})
-		}
-
-		pmapi.Apidb, pmapi.ApiErr = pmapi.PMconn.OpenConn()
-		if pmapi.ApiErr != nil {
-			c.JSON(http.StatusExpectationFailed, gin.H{"error": pmapi.ApiErr})
-		}
-
-		if pmapi.ApiErr = c.Bind(&tmpusr); pmapi.ApiErr != nil {
-			c.JSON(http.StatusExpectationFailed, gin.H{"result": pmapi.ApiErr})
-		}
-		log.Print("pmapi->CreateOneUser->AddOneUser tmpusr", tmpusr)
-
-		pmapi.ApiErr = tmpusr.AddOneUser(pmapi.Apidb)
-		if pmapi.ApiErr != nil {
-			log.Print("pmapi->CreateOneUser->AddOneUser Failed", pmapi.ApiErr)
-			c.JSON(http.StatusExpectationFailed, gin.H{"result": pmapi.ApiErr})
+			c.JSON(http.StatusInternalServerError, errors.ErrorStack(pmapi.ApiErr))
 		} else {
-			c.JSON(http.StatusOK, gin.H{"result": "OK"})
+			pmapi.Apidb, pmapi.ApiErr = pmapi.PMconn.OpenConn()
+			if pmapi.ApiErr != nil {
+				c.JSON(http.StatusInternalServerError, errors.ErrorStack(pmapi.ApiErr))
+			} else {
+				if pmapi.ApiErr = c.Bind(&tmpusr); pmapi.ApiErr != nil {
+					c.JSON(http.StatusExpectationFailed, errors.ErrorStack(pmapi.ApiErr))
+				} else {
+
+					pmapi.ApiErr = tmpusr.AddOneUser(pmapi.Apidb)
+					if pmapi.ApiErr != nil {
+						c.JSON(http.StatusInternalServerError, errors.ErrorStack(pmapi.ApiErr))
+					} else {
+						c.JSON(http.StatusOK, gin.H{"exit": "0", "messages": tmpusr.Username + " Create Successed!"})
+					}
+
+				}
+			}
+
 		}
+
 	}
 }
 
@@ -136,22 +138,26 @@ func (pmapi *PMApi) ListAllUsers(c *gin.Context) {
 				pmapi.PMconn, pmapi.ApiErr = proxysql.NewConn(pmapi.PMhost, pmapi.PMport, pmapi.PMuser, pmapi.PMpass)
 				if pmapi.ApiErr != nil {
 					c.JSON(http.StatusInternalServerError, errors.ErrorStack(pmapi.ApiErr))
+				} else {
+					// Open Connection.
+					pmapi.Apidb, pmapi.ApiErr = pmapi.PMconn.OpenConn()
+					if pmapi.ApiErr != nil {
+						c.JSON(http.StatusInternalServerError, errors.ErrorStack(pmapi.ApiErr))
+					} else {
+						//Execute Query.
+						aryusr, pmapi.ApiErr = proxysql.FindAllUserInfo(pmapi.Apidb, limit, skip)
+						if pmapi.ApiErr != nil {
+							c.JSON(http.StatusInternalServerError, errors.ErrorStack(pmapi.ApiErr))
+						} else {
+
+							// return success.
+							c.JSON(http.StatusOK, aryusr)
+						}
+
+					}
+
 				}
 
-				// Open Connection.
-				pmapi.Apidb, pmapi.ApiErr = pmapi.PMconn.OpenConn()
-				if pmapi.ApiErr != nil {
-					c.JSON(http.StatusInternalServerError, errors.ErrorStack(pmapi.ApiErr))
-				}
-
-				//Execute Query.
-				aryusr, pmapi.ApiErr = proxysql.FindAllUserInfo(pmapi.Apidb, limit, skip)
-				if pmapi.ApiErr != nil {
-					c.JSON(http.StatusInternalServerError, errors.ErrorStack(pmapi.ApiErr))
-				}
-
-				// return success.
-				c.JSON(http.StatusOK, aryusr)
 			}
 
 		}
@@ -175,31 +181,37 @@ func (pmapi *PMApi) UpdateOneUser(c *gin.Context) {
 	if hostname == "" || hostname == "undefined" || port == "" || port == "undefined" || username == "" || username == "undefined" || password == "" || password == "undefined" {
 		c.JSON(http.StatusBadRequest, errors.ErrorStack(errors.New("hostname|port|adminuser|adminpass length is 0 or value is undefined")))
 	} else {
-		pmapi.PMhost = hostname + ":" + port
+		pmapi.PMhost = hostname
+		pmapi.PMport, _ = strconv.ParseUint(port, 10, 64)
 		pmapi.PMuser = username
 		pmapi.PMpass = password
 
+		// New connection instance.
 		pmapi.PMconn, pmapi.ApiErr = proxysql.NewConn(pmapi.PMhost, pmapi.PMport, pmapi.PMuser, pmapi.PMpass)
 		if pmapi.ApiErr != nil {
-			c.JSON(http.StatusExpectationFailed, gin.H{"error": pmapi.ApiErr})
-		}
-
-		pmapi.Apidb, pmapi.ApiErr = pmapi.PMconn.OpenConn()
-		if pmapi.ApiErr != nil {
-			c.JSON(http.StatusExpectationFailed, gin.H{"error": pmapi.ApiErr})
-		}
-
-		if pmapi.ApiErr = c.Bind(&tmpusr); pmapi.ApiErr != nil {
-			c.JSON(http.StatusExpectationFailed, gin.H{"result": pmapi.ApiErr})
-		}
-		log.Print("pmapi->UpdateOneUser->UpdateOneUser tmpusr", tmpusr)
-
-		pmapi.ApiErr = tmpusr.UpdateOneUserInfo(pmapi.Apidb)
-		if pmapi.ApiErr != nil {
-			log.Print("pmapi->UpdateOneUser->UpdateOneUser Failed", pmapi.ApiErr)
-			c.JSON(http.StatusExpectationFailed, gin.H{"result": pmapi.ApiErr})
+			c.JSON(http.StatusInternalServerError, errors.ErrorStack(pmapi.ApiErr))
 		} else {
-			c.JSON(http.StatusOK, gin.H{"result": "OK"})
+			// Open Connection.
+			pmapi.Apidb, pmapi.ApiErr = pmapi.PMconn.OpenConn()
+			if pmapi.ApiErr != nil {
+				c.JSON(http.StatusInternalServerError, errors.ErrorStack(pmapi.ApiErr))
+			} else {
+				// get args.
+				if pmapi.ApiErr = c.Bind(&tmpusr); pmapi.ApiErr != nil {
+					c.JSON(http.StatusExpectationFailed, errors.ErrorStack(pmapi.ApiErr))
+				} else {
+					pmapi.ApiErr = tmpusr.UpdateOneUserInfo(pmapi.Apidb)
+					if pmapi.ApiErr != nil {
+						c.JSON(http.StatusInternalServerError, errors.ErrorStack(pmapi.ApiErr))
+					} else {
+						c.JSON(http.StatusOK, gin.H{"exit": "0", "messages": tmpusr.Username + " Update Successed!"})
+					}
+
+				}
+
+			}
+
 		}
+
 	}
 }
