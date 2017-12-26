83bb0f4c14845a2193244a1ecb6b2d5fe26fb6f1
Lei Tian <taylor840326@gmail.com>
2017-12-26 14:14:54 +0800

    update DeleteOneServer to catch some exception

--- pmapi/servers.go
+++ pmapi/servers.go
@@ -132,34 +132,41 @@ func (pmapi *PMApi) DeleteOneServers(c *gin.Context) {
 	username := c.Query("adminuser")
 	password := c.Query("adminpass")
 
-	if len(hostname) == 0 {
-		c.JSON(http.StatusOK, []proxysql.Servers{})
+	if hostname == "" || hostname == "undefined" || port == "" || port == "undefined" || username == "" || username == "undefined" || password == "" || password == "undefined" {
+		c.JSON(http.StatusBadRequest, errors.ErrorStack(errors.New("hostname|port|adminuser|adminpass length is 0 or value is undefined")))
 	} else {
-		pmapi.PMhost = hostname + ":" + port
+		pmapi.PMhost = hostname
+		pmapi.PMport, _ = strconv.ParseUint(port, 10, 64)
 		pmapi.PMuser = username
 		pmapi.PMpass = password
 
 		pmapi.PMconn, pmapi.ApiErr = proxysql.NewConn(pmapi.PMhost, pmapi.PMport, pmapi.PMuser, pmapi.PMpass)
 		if pmapi.ApiErr != nil {
-			c.JSON(http.StatusExpectationFailed, gin.H{"error": pmapi.ApiErr})
-		}
-
-		pmapi.Apidb, pmapi.ApiErr = pmapi.PMconn.OpenConn()
-		if pmapi.ApiErr != nil {
-			c.JSON(http.StatusExpectationFailed, gin.H{"error": pmapi.ApiErr})
-		}
-
-		if pmapi.ApiErr = c.Bind(&tmpserver); pmapi.ApiErr != nil {
-			c.JSON(http.StatusExpectationFailed, gin.H{"result": pmapi.ApiErr})
-		}
-		log.Print("pmapi->DeleteOneServer->DeleteOneServer tmpserver", tmpserver)
-
-		pmapi.ApiErr = tmpserver.DeleteOneServers(pmapi.Apidb)
-		if pmapi.ApiErr != nil {
-			log.Print("pmapi->DeleteOneServer->DeleteOneServer Failed", pmapi.ApiErr)
-			c.JSON(http.StatusExpectationFailed, gin.H{"result": pmapi.ApiErr})
+			c.JSON(http.StatusInternalServerError, errors.ErrorStack(pmapi.ApiErr))
 		} else {
-			c.JSON(http.StatusOK, gin.H{"result": "OK"})
+			pmapi.Apidb, pmapi.ApiErr = pmapi.PMconn.OpenConn()
+			if pmapi.ApiErr != nil {
+				c.JSON(http.StatusInternalServerError, errors.ErrorStack(pmapi.ApiErr))
+			} else {
+
+				if pmapi.ApiErr = c.Bind(&tmpserver); pmapi.ApiErr != nil {
+					c.JSON(http.StatusExpectationFailed, errors.ErrorStack(pmapi.ApiErr))
+				} else {
+					delsrv, _ := proxysql.NewServer(tmpserver.HostGroupId, tmpserver.HostName, tmpserver.Port)
+					pmapi.ApiErr = delsrv.DeleteOneServers(pmapi.Apidb)
+					if pmapi.ApiErr != nil {
+						switch {
+						case errors.IsNotFound(pmapi.ApiErr):
+							c.JSON(http.StatusNotFound, errors.ErrorStack(pmapi.ApiErr))
+						default:
+							c.JSON(http.StatusInternalServerError, errors.ErrorStack(pmapi.ApiErr))
+						}
+					} else {
+						c.JSON(http.StatusOK, gin.H{"exit": "0", "messages": strconv.Itoa(int(tmpserver.HostGroupId)) + "-" + tmpserver.HostName + "-"+strconv.Itoa(int(tmpserver.Port))+ " Delete Successed!"})
+					}
+
+				}
+			}
 		}
 	}
 }
