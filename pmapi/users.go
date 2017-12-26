1879576e8c730b4b944e68fccd935acfa5a2ccc4
Lei Tian <taylor840326@gmail.com>
2017-12-26 11:43:00 +0800

    update DeleteOneUser in users.go

--- pmapi/users.go
+++ pmapi/users.go
@@ -20,8 +20,8 @@ func (pmapi *PMApi) DeleteOneUser(c *gin.Context) {
 	username := c.Query("adminuser")
 	password := c.Query("adminpass")
 
-	if len(hostname) == 0 {
-		c.JSON(http.StatusOK, []proxysql.Users{})
+	if hostname == "" || hostname == "undefined" || port == "" || port == "undefined" || username == "" || username == "undefined" || password == "" || password == "undefined" {
+		c.JSON(http.StatusBadRequest, errors.ErrorStack(errors.New("hostname|port|adminuser|adminpass length is 0 or value is undefined")))
 	} else {
 		pmapi.PMhost = hostname
 		pmapi.PMport, _ = strconv.ParseUint(port, 10, 64)
@@ -30,26 +30,28 @@ func (pmapi *PMApi) DeleteOneUser(c *gin.Context) {
 
 		pmapi.PMconn, pmapi.ApiErr = proxysql.NewConn(pmapi.PMhost, pmapi.PMport, pmapi.PMuser, pmapi.PMpass)
 		if pmapi.ApiErr != nil {
-			c.JSON(http.StatusExpectationFailed, gin.H{"error": pmapi.ApiErr})
-		}
-
-		pmapi.Apidb, pmapi.ApiErr = pmapi.PMconn.OpenConn()
-		if pmapi.ApiErr != nil {
-			c.JSON(http.StatusExpectationFailed, gin.H{"error": pmapi.ApiErr})
-		}
-
-		if pmapi.ApiErr = c.Bind(&tmpusr); pmapi.ApiErr != nil {
-			c.JSON(http.StatusExpectationFailed, gin.H{"result": pmapi.ApiErr})
-		}
-		log.Print("pmapi->DeleteOneUser->DeleteOneUser tmpusr", tmpusr)
-
-		pmapi.ApiErr = tmpusr.DeleteOneUser(pmapi.Apidb)
-		if pmapi.ApiErr != nil {
-			log.Print("pmapi->DeleteOneUser->DeleteOneUser Failed", pmapi.ApiErr)
-			c.JSON(http.StatusExpectationFailed, gin.H{"result": pmapi.ApiErr})
+			c.JSON(http.StatusInternalServerError, errors.ErrorStack(pmapi.ApiErr))
 		} else {
-			c.JSON(http.StatusOK, gin.H{"result": "OK"})
+			pmapi.Apidb, pmapi.ApiErr = pmapi.PMconn.OpenConn()
+			if pmapi.ApiErr != nil {
+				c.JSON(http.StatusInternalServerError, errors.ErrorStack(pmapi.ApiErr))
+			} else {
+				if pmapi.ApiErr = c.Bind(&tmpusr); pmapi.ApiErr != nil {
+					c.JSON(http.StatusExpectationFailed, errors.ErrorStack(pmapi.ApiErr))
+				} else {
+					pmapi.ApiErr = tmpusr.DeleteOneUser(pmapi.Apidb)
+					if pmapi.ApiErr != nil {
+						c.JSON(http.StatusInternalServerError, errors.ErrorStack(pmapi.ApiErr))
+					} else {
+						c.JSON(http.StatusOK, gin.H{"exit": "0", "messages": tmpusr.Username + " Delete Successed!"})
+					}
+
+				}
+
+			}
+
 		}
+
 	}
 }
 
