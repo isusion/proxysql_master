ff113ad43e992528cec61d1f305d109a16deaefa
Lei Tian <taylor840326@gmail.com>
2017-12-26 14:38:29 +0800

    update ListAllQueryRules to catch some exception in queryrules.go

--- pmapi/queryrules.go
+++ pmapi/queryrules.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/imSQL/proxysql"
+	"github.com/juju/errors"
 )
 
 //查询出所有查询规则
@@ -18,44 +19,58 @@ func (pmapi *PMApi) ListAllQueryRules(c *gin.Context) {
 	port := c.Query("port")
 	username := c.Query("adminuser")
 	password := c.Query("adminpass")
-	limit, _ := strconv.ParseInt(c.Query("limit"), 10, 64)
-	page, _ := strconv.ParseInt(c.Query("page"), 10, 64)
-
-	if limit == 0 {
-		limit = 10
-	}
-
-	if page == 0 {
-		page = 1
-	}
-
-	skip := (page - 1) * limit
-
-	if len(hostname) == 0 || hostname == "undefined" {
-		c.JSON(http.StatusOK, []proxysql.QueryRules{})
+	limit, err := strconv.ParseUint(c.Query("limit"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, errors.ErrorStack(errors.NewBadRequest(err, "limit  must >= 0")))
 	} else {
-		pmapi.PMhost = hostname
-		pmapi.PMport, _ = strconv.ParseUint(port, 10, 64)
-		pmapi.PMuser = username
-		pmapi.PMpass = password
+		page, err := strconv.ParseUint(c.Query("page"), 10, 64)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, errors.ErrorStack(errors.NewBadRequest(err, "page must > 0")))
+		} else {
 
-		pmapi.PMconn, pmapi.ApiErr = proxysql.NewConn(pmapi.PMhost, pmapi.PMport, pmapi.PMuser, pmapi.PMpass)
-		if pmapi.ApiErr != nil {
-			c.JSON(http.StatusExpectationFailed, gin.H{"error": pmapi.ApiErr})
-		}
+			if limit == 0 {
+				limit = 10
+			}
 
-		pmapi.Apidb, pmapi.ApiErr = pmapi.PMconn.OpenConn()
-		if pmapi.ApiErr != nil {
-			c.JSON(http.StatusExpectationFailed, gin.H{"error": pmapi.ApiErr})
-		}
+			if page == 0 {
+				page = 1
+			}
 
-		aryqrs, pmapi.ApiErr = proxysql.FindAllQr(pmapi.Apidb, limit, skip)
-		if pmapi.ApiErr != nil {
-			c.JSON(http.StatusExpectationFailed, gin.H{"error": pmapi.ApiErr})
+			skip := (page - 1) * limit
+
+			if hostname == "" || hostname == "undefined" || port == "" || port == "undefined" || username == "" || username == "undefined" || password == "" || password == "undefined" {
+				c.JSON(http.StatusBadRequest, errors.ErrorStack(errors.New("hostname|port|adminuser|adminpass length is 0 or value is undefined")))
+			} else {
+				log.Printf(hostname)
+				pmapi.PMhost = hostname
+				pmapi.PMport, _ = strconv.ParseUint(port, 10, 64)
+				pmapi.PMuser = username
+				pmapi.PMpass = password
+
+				// New connection instance
+				pmapi.PMconn, pmapi.ApiErr = proxysql.NewConn(pmapi.PMhost, pmapi.PMport, pmapi.PMuser, pmapi.PMpass)
+				if pmapi.ApiErr != nil {
+					c.JSON(http.StatusInternalServerError, errors.ErrorStack(pmapi.ApiErr))
+				} else {
+					// Open Connection.
+					pmapi.Apidb, pmapi.ApiErr = pmapi.PMconn.OpenConn()
+					if pmapi.ApiErr != nil {
+						c.JSON(http.StatusInternalServerError, errors.ErrorStack(pmapi.ApiErr))
+					} else {
+
+						aryqrs, pmapi.ApiErr = proxysql.FindAllQr(pmapi.Apidb, limit, skip)
+						if pmapi.ApiErr != nil {
+							c.JSON(http.StatusInternalServerError, errors.ErrorStack(pmapi.ApiErr))
+						} else {
+
+							// return success.
+							c.JSON(http.StatusOK, aryqrs)
+						}
+					}
+				}
+			}
 		}
-		c.JSON(http.StatusOK, aryqrs)
 	}
-
 }
 
 func (pmapi *PMApi) CreateOneQueryRules(c *gin.Context) {
